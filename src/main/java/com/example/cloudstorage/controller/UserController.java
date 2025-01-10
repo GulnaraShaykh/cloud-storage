4f90fef4ab1310910b6495eb57ce9d93242471ac
Шайхутдинова Гульнара <gulnara.shaykhutdinova@gazprombank.ru>
2025-01-10 17:37:49 +0300

    WIP

--- src/main/java/com/example/cloudstorage/controller/UserController.java
+++ src/main/java/com/example/cloudstorage/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.cloudstorage.dto.*;
 import com.example.cloudstorage.model.User;
 import com.example.cloudstorage.service.AuthService;
 import com.example.cloudstorage.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
     @Autowired
@@ -22,20 +27,31 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        logger.info("Received login request for user: {}", loginRequest.getLogin());
+
         User user = userService.findUserByLogin(loginRequest.getLogin());
+
         if (user == null) {
+            logger.warn("User not found for login: {}", loginRequest.getLogin());
+            ErrorResponse errorResponse = new ErrorResponse();
+            errorResponse.setMessage("Invalid login or password");
+            errorResponse.setId(400);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        } else if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            logger.warn("Incorrect password for user: {}", loginRequest.getLogin());
             ErrorResponse errorResponse = new ErrorResponse();
             errorResponse.setMessage("Invalid login or password");
             errorResponse.setId(400);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         } else {
+            logger.info("User logged in successfully: {}", loginRequest.getLogin());
+            // Генерация токена
             String authToken = authService.generateAuthToken(user);
             LoginResponse loginResponse = new LoginResponse();
             loginResponse.setAuthToken(authToken);
-            return ResponseEntity.ok(loginResponse);
+            return ResponseEntity.ok(loginResponse);  // Отправляем токен в ответе
         }
     }
-
 
     @PostMapping("/logout")
     public ResponseEntity<?> logout(@RequestHeader("auth-token") String authToken) {
