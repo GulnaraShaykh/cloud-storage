2e79946e1fd9020f0b52822f8d97e22a4d568f83
Шайхутдинова Гульнара <gulnara.shaykhutdinova@gazprombank.ru>
2025-01-17 18:20:27 +0300

    WIP

--- src/main/java/com/example/cloudstorage/model/User.java
+++ src/main/java/com/example/cloudstorage/model/User.java
@@ -1,8 +1,15 @@
 package com.example.cloudstorage.model;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,35 +25,9 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    public long getId() {
-        return id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
+    public User(String userName, String login, String password) {
         this.userName = userName;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setLogin(String login) {
         this.login = login;
+        this.password = password;
     }
 }
